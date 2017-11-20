a25f9aa6eb4eb1972033eb887931897e04b8b0de
Craige McWhirter <craige@mcwhirter.io>
2017-11-20 18:50:17 +1000

    Finished deck and add SLAB walls

--- the_house.py
+++ the_house.py
@@ -16,6 +16,8 @@ HOUSE_FLOOR_SLAB_X = list(range(3, 20))
 HOUSE_FLOOR_SLAB_Z = list(range(-1, 8))
 HOUSE_FLOOR_WOOD_X = list(range(7, 20))
 HOUSE_FLOOR_WOOD_Z = list(range(8, 12))
+HOUSE_FLOOR_DECK_X = list(range(7, 20))
+HOUSE_FLOOR_DECK_Z = list(range(12, 16))
 
 # Grounds
 GROUNDS_Y = POS.y - 1
@@ -27,8 +29,9 @@ GROUNDS_Z2 = POS.z + 27
 # Set blocks
 CLEAR = block.AIR.id
 GROUNDS = block.GRASS.id
-HOUSE_FLOOR_SLAB = block.STONE.id
-HOUSE_FLOOR_WOOD = block.WOOD_PLANKS.id, 1
+HOUSE_FLOOR_SLAB_BLOCK = block.STONE.id
+HOUSE_FLOOR_WOOD_BLOCK = block.WOOD_PLANKS.id, 3
+HOUSE_FLOOR_DECK_BLOCK = block.WOOD_PLANKS.id, 4
 
 def clear_space():
     ''' Clear a space for the house by setting it to AIR '''
@@ -65,7 +68,7 @@ def build_floor():
         POS.x + HOUSE_FLOOR_SLAB_X[-1],
         GROUNDS_Y,
         POS.z + HOUSE_FLOOR_SLAB_Z[-1],
-        HOUSE_FLOOR_SLAB
+        HOUSE_FLOOR_SLAB_BLOCK
         )
     # Build the wooden floor
     MC.setBlocks(
@@ -75,9 +78,44 @@ def build_floor():
         POS.x + HOUSE_FLOOR_WOOD_X[-1],
         GROUNDS_Y,
         POS.z + HOUSE_FLOOR_WOOD_Z[-1],
-        HOUSE_FLOOR_WOOD
+        HOUSE_FLOOR_WOOD_BLOCK
         )
+    # Build the wooden deck
+    MC.setBlocks(
+        POS.x + HOUSE_FLOOR_DECK_X[0],
+        GROUNDS_Y,
+        POS.z + HOUSE_FLOOR_DECK_Z[0],
+        POS.x + HOUSE_FLOOR_DECK_X[-1],
+        GROUNDS_Y,
+        POS.z + HOUSE_FLOOR_DECK_Z[-1],
+        HOUSE_FLOOR_DECK_BLOCK
+        )
+
+def build_walls():
+    ''' Build all the walls'''
+    # Build the slab walls
+    MC.setBlocks(
+        POS.x + HOUSE_FLOOR_SLAB_X[0],
+        GROUNDS_Y + 1,
+        POS.z + HOUSE_FLOOR_SLAB_Z[0],
+        POS.x + HOUSE_FLOOR_SLAB_X[-1],
+        GROUNDS_Y + 4,
+        POS.z + HOUSE_FLOOR_SLAB_Z[-1],
+        HOUSE_FLOOR_SLAB_BLOCK
+        )
+    # Clear inside the slab walls
+    MC.setBlocks(
+        POS.x + HOUSE_FLOOR_SLAB_X[0] + 1,
+        GROUNDS_Y + 1,
+        POS.z + HOUSE_FLOOR_SLAB_Z[0] + 1,
+        POS.x + HOUSE_FLOOR_SLAB_X[-1] - 1,
+        GROUNDS_Y + 4,
+        POS.z + HOUSE_FLOOR_SLAB_Z[-1] - 1,
+        CLEAR
+        )
+
 
 clear_space()
 build_grounds()
 build_floor()
+build_walls()
